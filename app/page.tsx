0eca80c82439c5e44d70e3dbbec7a8505827c6d4
Gustavo Ramirez <tavo.joy.36@gmail.com>
2025-03-22 09:39:15 -0400

    added card

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import { Bar, BarChart, CartesianGrid, XAxis, Tooltip, Legend, YAxis } from "recharts"
 import { Rnd } from 'react-rnd'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 export default function Home() {
   const [isMounted, setIsMounted] = useState(false)
@@ -20,15 +21,24 @@ export default function Home() {
 
   return (
     <Rnd>
-      <BarChart width={300} height={180} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="pv" fill="#8884d8" />
-        <Bar dataKey="uv" fill="#82ca9d" />
-      </BarChart>
+      <Card
+        className={``}
+      >
+        <CardHeader>
+          <CardTitle>Title</CardTitle>
+        </CardHeader>
+        <CardContent className="pb-4">
+          <BarChart width={300} height={180} data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
+        </CardContent>
+      </Card>
     </Rnd>
   )
 }
