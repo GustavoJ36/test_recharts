2047644ea0380f843630e59d075524421384594b
Gustavo Ramirez <tavo.joy.36@gmail.com>
2025-03-22 08:53:26 -0400

    simplified

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 "use client"
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  Tooltip,
+  Legend,
+  YAxis
+} from "recharts"
 
 export default function Home() {
   const data = [
@@ -40,10 +49,16 @@ export default function Home() {
   ]
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        {data[0].name}
-      </main>
-    </div>
+    <>
+      <BarChart width={730} height={250} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="pv" fill="#8884d8" />
+        <Bar dataKey="uv" fill="#82ca9d" />
+      </BarChart>
+    </>
   );
 }
