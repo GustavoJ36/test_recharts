e394056fe1e47dc2fdfe4fa171ea87879d8711c8
Gustavo Ramirez <tavo.joy.36@gmail.com>
2025-04-07 14:03:10 -0400

    fixed delete button position

--- app/page.tsx
+++ app/page.tsx
@@ -40,8 +40,7 @@ export default function Home() {
 
   <button
   onClick={() => ChartDeleteHandler()}
-  className="absolute t-right-1 top-8 z-50 rounded-full w-6 h-6 flex items-center justify-center shadow-sm"
-
+  className="absolute -right-2 -top-2 z-50 rounded-full w-6 h-6 flex items-center justify-center shadow-sm bg-white hover:bg-gray-100 text-gray-500 hover:text-gray-700"
   aria-label="Delete chart"
   >
   ×
